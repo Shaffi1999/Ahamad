e4d692e74aa3fd0f6d13fd2f9774e0c2ddf0f21b
M1083096 <M1083096@MT-9YDV8Y2>
2022-04-26 09:57:46 +0530

    Updated File

--- Demo.java
+++ Demo.java
@@ -4,6 +4,7 @@ public static void main(String args[])
 {
 	int a=10,b=20,c;
 	c=a+b;
-	System.out.println("Thw sum is:"+c);
+	System.out.println("The sum is:"+c);
+	System.out.println("Hello world");
 }
 }
